87b897b8c20737483089d083b2504a4263736ffb
geetapingale16 <146621299+geetapingale16@users.noreply.github.com>
2023-10-02 02:20:39 -0700

    Add files via upload
    golden project exam seating arrangement system in java

---
+++ Query (1).sql
@@ -0,0 +1,15 @@
+create database library;
+drop database library;
+
+select * from books;
+insert into books value
+(111,"Fundamentals of Wavelets","Goswami",1),(112,"Data Smart","Foreman",1),
+(113,"God Created the Integers","Hawking",1),(114,"Superfreakonomics","Dubner",1),
+(115,"Orientalism","Edward",1),(116,"Jurassic Park","Michael",1),
+(117,"Freakonomics","Dubner",1),(118,"City of Joy","Dominique",1),
+(119,"Free Will","Harris",1),(120,"Electric Universe","Bodanis",1);
+
+select * from students;
+insert into students value
+(1001,"Vedant",1),(1002,"Vipul",2),(1003,"Ankit",4),
+(1004,"Akash",0),(1005,"Raghav",1),(1006,"Akshay",0);
